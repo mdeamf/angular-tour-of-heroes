e331c1bae91d254cad6153328fb748c8a55c58c0
Mauricio Francisco <mauricio.francisco@gft.com>
2020-11-23 10:42:00 -0300

    toh-pt4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +10,23 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
+  heroes: Hero[];
   selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
-  constructor() { }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
